7bddf613c378173ee5482122aeeeff7a35abff1d
Matthias Mittner <mittner@uva.nl>
2013-10-16 16:52:46 +0200

    works without hardcoded path to ipython

--- IPythonQL/GeneratePreviewForURL.m
+++ IPythonQL/GeneratePreviewForURL.m
@@ -9,11 +9,6 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
 void CancelPreviewGeneration(void *thisInterface, QLPreviewRequestRef preview);
 bool debug=false;
 
-/* -----------------------------------------------------------------------------
- Generate a preview for file
- 
- This function's job is to create preview for designated file
- ----------------------------------------------------------------------------- */
 void FixUnixPath() {
     // adapted from http://stackoverflow.com/questions/208897/find-out-location-of-an-executable-file-in-cocoa
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^(void){
@@ -44,48 +39,79 @@ void FixUnixPath() {
         NSString* newStr = [NSString stringWithUTF8String:[data bytes]];
         NSString *userPath=[newStr stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 
-        /*
+        /* this doesn't work for some reason; I use the preceding which is just a more verbose version of that line*/
+         /*
         NSString *userPath = [[NSTask stringByLaunchingPath:userShell withArguments:[NSArray arrayWithObjects:@"-c", @"echo $PATH", nil] error:nil]
                               stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
          */
         if (userPath.length > 0 && [userPath rangeOfString:@":"].length > 0 && [userPath rangeOfString:@"/usr/bin"].length > 0) {
             // BINGO!
-            if(debug) NSLog(@"User's PATH as reported by %@ is %@", userShell, userPath);
+            if(debug) NSLog(@"User's PATH as reported by '%@' is '%@'", userShell, userPath);
             setenv("PATH", [userPath fileSystemRepresentation], 1);
         }
     });
 }
 
+NSString* getIPython(){
+        NSString *userShell = [[[NSProcessInfo processInfo] environment] objectForKey:@"SHELL"];
+        if(debug) NSLog(@"User's shell is %@", userShell);
+        
+        // avoid executing stuff like /sbin/nologin as a shell
+        BOOL isValidShell = NO;
+        for (NSString *validShell in [[NSString stringWithContentsOfFile:@"/etc/shells" encoding:NSUTF8StringEncoding error:nil] componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]]) {
+            if ([[validShell stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] isEqualToString:userShell]) {
+                isValidShell = YES;
+                break;
+            }
+        }
+        
+        if (!isValidShell) {
+            if(debug) NSLog(@"Shell %@ is not in /etc/shells, won't continue.", userShell);
+        }
+        NSTask *task=[[NSTask alloc] init];
+        [task setLaunchPath: userShell];
+        [task setArguments: [NSArray arrayWithObjects: @"-l", @"-c", @"which ipython", nil]];
+        NSPipe *pipe=[[NSPipe alloc] init];
+        [task setStandardOutput:pipe];
+        NSFileHandle *file = [pipe fileHandleForReading];
+        [task launch];
+        NSData *data = [file readDataToEndOfFile];
+        NSString* ipython = [NSString stringWithUTF8String:[data bytes]];
+        ipython=[ipython stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        ipython=[ipython stringByReplacingOccurrencesOfString:@"\n" withString:@""];
+    
+        if(debug) NSLog(@"fct(IPython)='%@'", ipython);
+        return ipython;
+}
+/* -----------------------------------------------------------------------------
+ Generate a preview for file
+ 
+ This function's job is to create preview for designated file
+ ----------------------------------------------------------------------------- */
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options) {
     @autoreleasepool {
         if (QLPreviewRequestIsCancelled(preview))
             return noErr;
 
         NSURL *url2=(__bridge NSURL*) url;
-        
-        if(debug) NSLog(@"inurl = %@",url2); // its printing null
-
+        if(debug) NSLog(@"inurl = %@",url2);
         NSString *fullpath=[url2 path];
     
+        FixUnixPath();
+
         NSTask *task;
         task = [[NSTask alloc] init];
-        //[task setLaunchPath: @"bash"];
-        [task setLaunchPath: @"/Library/Frameworks/Python.framework/Versions/2.7/bin/ipython"];
+        NSString *ipython=getIPython();
+        if(debug) NSLog(@"IPython='%@'", ipython);
+        
+//        ipython=@"/Library/Frameworks/Python.framework/Versions/2.7/bin/ipython";
 
+        [task setLaunchPath: ipython]; //@"ipython"];
+        
         NSArray *arguments;
-//        arguments = [NSArray arrayWithObjects: @"nbconvert", @"--to", @"html", @"--stdout", fullpath, nil];
         arguments = [NSArray arrayWithObjects: @"nbconvert", @"--to", @"html", @"--output=/tmp/blubberdiblubb123", fullpath, nil];
 
-  
-        FixUnixPath();
-//        NSDictionary *env=[[NSProcessInfo processInfo]environment];
-//        [env setValue:@"/usr/local/bin" forKey: @"PATH"];
-//        env[@"PATH"]="/usr/local/bin";
-//        if(debug) NSLog(@"env=%@", env);
-        
-        
         [task setArguments: arguments];
-//        [task setEnvironment:env];
         if(debug) NSLog(@"task=%@", [task arguments] );
         
 
@@ -117,9 +143,6 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
         NSURL *ourl = [NSURL fileURLWithPath:@"/tmp/blubberdiblubb123.html"];
         NSString *htmlcontent = [NSString stringWithContentsOfURL:ourl encoding:NSUTF8StringEncoding error:nil];
         
-//        NSString *htmlcontent;
-  //      htmlcontent = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
-    
         // Put metadata and attachment in a dictionary
         NSDictionary *properties = @{ // properties for the HTML data
                                      (__bridge NSString *)kQLPreviewPropertyTextEncodingNameKey : @"UTF-8",
